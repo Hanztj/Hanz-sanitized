b2da75759b954d38a90ab7bcc4f40237b8bbb6ca
Hanztj <hanz2hk@gmail.com>
2026-04-15 22:27:59 +0100

    Create commercial_flashing_catalog.dart

---
+++ lib/Screens/commercial/components/commercial_flashing_catalog.dart
@@ -0,0 +1,12 @@
+// lib/screens/commercial/components/commercial_flashing_catalog.dart
+
+const Map<String, List<String>> tpoFlashingOptions = {
+  'Flash Parapet wall': ['Up to 3\'', 'Over 3\' up to 6\''],
+  'Curb flashing': [], // usa TextField
+  'Cap flashing': ['Average', 'Large'],
+  'Skylight flashing kit (dome)': ['Average', 'Large'],
+  'Skylight Step Flashing': ['Up to 5 sf', '5.1 to 12 sf', '12.1 sf or greater'],
+};
+
+const List<String> capFlashingMaterials = ['Metal', 'Copper', 'Steel'];
+const List<String> skylightGrades = ['Standard', 'High grade'];
